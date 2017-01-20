e0d3e086ccc9b890a5ddb2add8bf42044f4e95ee
Dinesh Krishnan <dinaki08tt@gmail.com>
2017-01-21 00:27:43 +0530

    Local Storage poc added

---
+++ testapp/src/main/webapp/js/localService.js
@@ -0,0 +1,29 @@
+
+function getAllItems(){
+	return ['item a','item b','item c','item d','item e','item f','item g'];
+	
+}
+
+var key = 'items';
+
+function clearLocalStorage(){
+	$.localStorage.remove(key);
+}
+
+function localSaveItem(item){
+
+var ret = $.localStorage.get(key);
+if(ret!=null)
+{
+	ret[ret.length] = item;
+}else{
+	ret = new Array();
+	ret[0] = item;
+}	
+	
+$.localStorage.set(key,JSON.stringify(ret));
+	
+	var ret = $.localStorage.get(key);
+	var instance = ret[0];
+	console.log("Object in localStorage: "+instance.name);
+}
