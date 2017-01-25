794a299383b15807de09058e68ad0d5bbff19717
Dinesh Krishnan <dinaki08tt@gmail.com>
2017-01-25 12:06:59 +0530

    skeleton for UI basic feature

---
+++ testapp/src/main/webapp/js/init.js
@@ -0,0 +1,10 @@
+function initItems(){
+	return [
+	        {id: 1,
+	        name: "pepsi"
+	        },
+	        {id : 2
+	        name: "tropicana"
+	        }
+	        ]
+}
--- testapp/src/main/webapp/js/localService.js
+++ testapp/src/main/webapp/js/localService.js
@@ -1,10 +1,31 @@
 
-
-
+function renderDetailView(){
+	//details object in localStorage key(item) -> detail(detail name, parent name, country)
+	
+	//getAllItemsDirectlyfromStorage()
+	//iterate each items
+		//take one item
+			//check details object for item
+			//if(true)
+				//render in UI (table row)
+			//else
+				//callWebservice(item)
+				//details object from server
+				//persist in localStorage
+				//render in UI (table row)
+	
+}
+function addItem(){
+	//localStorage add the items
+	//render in UI
+}
 
 function getAllItems(){
+	//get initial list from json file
+	//saveToLocalStorage
+	//getFrom localStorage and return
+	//return json;
 	return ['item a','item b','item c','item d','item e','item f','item g'];
-	
 }
 
 var key = 'items';
